f7e20ebc6ef59884ba74abd7bf4bfcb925155a54
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-14 16:18:02 +0100

    added like button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "media": "https://unsplash.it/300/300?image=171",
         "author": {
             "name": "Phil Mangione",
             "image": "https://unsplash.it/300/300?image=15"
@@ -13,7 +13,7 @@ const posts = [
     {
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
+        "media": "https://unsplash.it/300/300?image=112",
         "author": {
             "name": "Sofia Perlari",
             "image": "https://unsplash.it/300/300?image=10"
@@ -24,7 +24,7 @@ const posts = [
     {
         "id": 3,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
+        "media": "https://unsplash.it/300/300?image=234",
         "author": {
             "name": "Chiara Passaro",
             "image": "https://unsplash.it/300/300?image=20"
@@ -35,7 +35,7 @@ const posts = [
     {
         "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/300/300?image=24",
         "author": {
             "name": "Luca Formicola",
             "image": null
@@ -46,26 +46,135 @@ const posts = [
     {
         "id": 5,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
+        "media": "https://unsplash.it/300/300?image=534",
         "author": {
             "name": "Alessandro Sainato",
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "2021-07-05"
+    },
+    {
+        "id": 6,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=54",
+        "author": {
+            "name": "Diego Verdecchia",
+            "image": null
+        },
+        "likes": 526,
+        "created": "2021-04-01"
+    },
+    {
+        "id": 7,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=4",
+        "author": {
+            "name": "Carluccio Corai",
+            "image": "https://unsplash.it/300/300?image=260"
+        },
+        "likes": 156,
+        "created": "2021-07-03"
+    },
+    {
+        "id": 8,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=5",
+        "author": {
+            "name": "Pellegrino Caselotti",
+            "image": "https://unsplash.it/300/300?image=290"
+        },
+        "likes": 6,
+        "created": "2021-12-03"
+    },
+    {
+        "id": 9,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=6",
+        "author": {
+            "name": "Fidenzio Camata",
+            "image": "https://unsplash.it/300/300?image=260"
+        },
+        "likes": 46,
+        "created": "2021-04-13"
+    },
+    {
+        "id": 10,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=7",
+        "author": {
+            "name": "Abaco Armelloni",
+            "image": null
+        },
+        "likes": 561,
+        "created": "2021-06-06"
+    },
+    {
+        "id": 11,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=8",
+        "author": {
+            "name": "Duina Baresi",
+            "image": "https://unsplash.it/300/300?image=320"
+        },
+        "likes": 86,
+        "created": "2021-02-13"
+    },
+    {
+        "id": 12,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=9",
+        "author": {
+            "name": "Fannï Tuminelli",
+            "image": "https://unsplash.it/300/300?image=220"
+        },
+        "likes": 69,
+        "created": "2021-2-09"
+    },
+    {
+        "id": 13,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=110",
+        "author": {
+            "name": "Angioletta Gnetti",
+            "image": null
+        },
+        "likes": 420,
+        "created": "2021-11-05"
+    },
+    {
+        "id": 14,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=100",
+        "author": {
+            "name": "Preziosa Verlengia",
+            "image": "https://unsplash.it/300/300?image=27"
+        },
+        "likes": 90,
+        "created": "2021-04-02"
+    },
+    {
+        "id": 15,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/300/300?image=101",
+        "author": {
+            "name": "Anchise Quartesan",
+            "image": "https://unsplash.it/300/300?image=22"
+        },
+        "likes": 136,
+        "created": "2021-01-01"
     }
 ];
 
 
 let postCard = '';
-
 for (let i = 0 ; i < posts.length ; i++){
     postCard += `
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src="${posts[i].media}" alt="Author name">                    
+                    <img class="profile-pic" src="${posts[i].media}" alt="Author pic">                    
                 </div>
                 <div class="post-meta__data">
                     <div class="post-meta__author">${posts[i].author.name}</div>
@@ -75,12 +184,12 @@ for (let i = 0 ; i < posts.length ; i++){
         </div>
         <div class="post__text">${posts[i].content}</div>
         <div class="post__image">
-            <img src="${posts[i].author.image}" alt="author image">
+            <img src="${posts[i].author.image}" alt="">
         </div>
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" ${posts[i].id}>
+                    <a id="event-like-button" class="like-button js-like-button" ${posts[i].id}>
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
@@ -94,5 +203,12 @@ for (let i = 0 ; i < posts.length ; i++){
     `
 }
 
+
 const cardWrapper = document.getElementById("container");
 cardWrapper.innerHTML += postCard;
+
+
+    document.getElementById("event-like-button").addEventListener("click", function(){
+        const like = document.querySelector(".like-button__label");
+        like.classList.toggle("add-like");
+    });
